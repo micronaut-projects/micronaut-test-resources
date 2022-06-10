ecc0f1092646187b2572df06087150c95870d528
Cedric Champeau <cedric.champeau@gmail.com>
2022-06-10 09:35:45 +0200

    Oracle R2DBC only compatible with Java 11+

--- test-resources-r2dbc/test-resources-r2dbc-oracle-xe/src/test/groovy/io/micronaut/testresources/r2dbc/oracle/StandaloneStartOracleTest.groovy
+++ test-resources-r2dbc/test-resources-r2dbc-oracle-xe/src/test/groovy/io/micronaut/testresources/r2dbc/oracle/StandaloneStartOracleTest.groovy
@@ -4,8 +4,10 @@ import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import io.micronaut.testresources.jdbc.AbstractJDBCSpec
 import io.micronaut.testresources.jdbc.Book
 import jakarta.inject.Inject
+import spock.lang.IgnoreIf
 
 @MicronautTest(environments = ["standalone"] )
+@IgnoreIf({ !jvm.java11Compatible })
 class StandaloneStartOracleTest extends AbstractJDBCSpec {
     @Inject
     ReactiveBookRepository repository

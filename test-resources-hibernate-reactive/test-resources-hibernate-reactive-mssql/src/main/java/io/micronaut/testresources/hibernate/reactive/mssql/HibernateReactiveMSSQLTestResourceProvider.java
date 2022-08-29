30b062d9262bfbf53d59174a206fe609436f1bee
Cédric Champeau <cedric.champeau@gmail.com>
2022-08-29 17:05:15 +0200

    Add support for Hibernate reactive (#50)
    * Add support for Hibernate reactive (WIP)
    
    * Fix Java 8 build
    
    * Adjust with latest release
    
    * Fix package directory
    
    * Make checkstyle happy
    
    * Bump to Micronaut 3.6.x
    
    * Update base branch

---
+++ test-resources-hibernate-reactive/test-resources-hibernate-reactive-mssql/src/main/java/io/micronaut/testresources/hibernate/reactive/mssql/HibernateReactiveMSSQLTestResourceProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.testresources.hibernate.reactive.mssql;
+
+import io.micronaut.testresources.hibernate.reactive.core.AbstractHibernateReactiveTestResourceProvider;
+import io.micronaut.testresources.mssql.MSSQLTestResourceProvider;
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Map;
+
+import static io.micronaut.testresources.mssql.MSSQLTestResourceProvider.createMSSQLContainer;
+
+/**
+ * A test resource provider which will spawn a MSSQL test container.
+ */
+public class HibernateReactiveMSSQLTestResourceProvider extends AbstractHibernateReactiveTestResourceProvider<MSSQLServerContainer<?>> {
+
+    @Override
+    protected String getSimpleName() {
+        return "mssql";
+    }
+
+    @Override
+    protected String getDefaultImageName() {
+        return MSSQLTestResourceProvider.DEFAULT_IMAGE_NAME;
+    }
+
+    @Override
+    protected MSSQLServerContainer<?> createContainer(DockerImageName imageName, Map<String, Object> requestedProperties, Map<String, Object> testResourcesConfiguration) {
+        return createMSSQLContainer(imageName, getSimpleName(), testResourcesConfiguration);
+    }
+
+}

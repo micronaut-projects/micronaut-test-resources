745335c61f4c2cc596581993ef30b7578d248674
Cedric Champeau <cedric.champeau@gmail.com>
2023-03-23 13:19:10 +0100

    Re-enable Hibernate reactive support
    Closes #189

--- test-resources-bom/build.gradle.kts
+++ test-resources-bom/build.gradle.kts
@@ -2,14 +2,3 @@ plugins {
     id("io.micronaut.build.internal.test-resources-base")
     id("io.micronaut.build.internal.bom")
 }
-
-micronautBom {
-    suppressions {
-        acceptedLibraryRegressions.add("micronaut-test-resources-hibernate-reactive-core")
-        acceptedLibraryRegressions.add("micronaut-test-resources-hibernate-reactive-mssql")
-        acceptedLibraryRegressions.add("micronaut-test-resources-hibernate-reactive-mysql")
-        acceptedLibraryRegressions.add("micronaut-test-resources-hibernate-reactive-mariadb")
-        acceptedLibraryRegressions.add("micronaut-test-resources-hibernate-reactive-postgresql")
-        acceptedLibraryRegressions.add("micronaut-test-resources-hibernate-reactive-oracle-xe")
-    }
-}

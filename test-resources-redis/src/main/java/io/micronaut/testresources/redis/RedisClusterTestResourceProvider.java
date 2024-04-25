54e1adcd4017049003cd81accbb0fb353f9d3d2f
Tim Yates <tim.yates@gmail.com>
2024-04-25 09:29:42 +0100

    Explicit imports instead of star imports

--- test-resources-redis/src/main/java/io/micronaut/testresources/redis/RedisClusterTestResourceProvider.java
+++ test-resources-redis/src/main/java/io/micronaut/testresources/redis/RedisClusterTestResourceProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 original authors
+ * Copyright 2017-2024 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,12 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-import static io.micronaut.testresources.redis.RedisConfigurationSupport.*;
+import static io.micronaut.testresources.redis.RedisConfigurationSupport.findInitialPort;
+import static io.micronaut.testresources.redis.RedisConfigurationSupport.findIp;
+import static io.micronaut.testresources.redis.RedisConfigurationSupport.findMasterCount;
+import static io.micronaut.testresources.redis.RedisConfigurationSupport.findNotifyKeyspaceEvents;
+import static io.micronaut.testresources.redis.RedisConfigurationSupport.findSlavesPerMasterCount;
+import static io.micronaut.testresources.redis.RedisConfigurationSupport.isClusterMode;
 
 /**
  * A test resource provider which will spawn a Redis cluster test container.

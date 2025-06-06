33cc2b82b5102c6c0efad030701a2a0aa6fcde02
phil.hardwick <4949381+PhilHardwick@users.noreply.github.com>
2025-06-06 12:08:54 +0100

    Remove disabling binary check

--- test-resources-wiremock/build.gradle.kts
+++ test-resources-wiremock/build.gradle.kts
@@ -9,10 +9,3 @@ Provides core support for Wiremock test resources.
 dependencies {
     api(libs.managed.testcontainers.wiremock)
 }
-
-micronautBuild {
-    // new module, so disable binary check for now
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}

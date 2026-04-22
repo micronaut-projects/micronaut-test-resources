2b71cdd61163e8c075dc70b73df41369ac0a9123
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-04-22 20:29:50 +0000

    Align Couchbase testcontainers follow-up

--- test-resources-couchbase/src/test/groovy/io/micronaut/testresources/couchbase/CouchbaseTestResourceProviderTest.groovy
+++ test-resources-couchbase/src/test/groovy/io/micronaut/testresources/couchbase/CouchbaseTestResourceProviderTest.groovy
@@ -1,25 +1,22 @@
 package io.micronaut.testresources.couchbase
 
 import io.micronaut.testresources.core.Scope
+import io.micronaut.testresources.testcontainers.AbstractTestContainersSpec
 import io.micronaut.testresources.testcontainers.TestContainers
-import spock.lang.Specification
 
-class CouchbaseTestResourceProviderTest extends Specification {
+class CouchbaseTestResourceProviderTest extends AbstractTestContainersSpec {
     private final CouchbaseTestResourceProvider provider = new CouchbaseTestResourceProvider()
-    private final Map<String, Object> requestedProperties = [(Scope.PROPERTY_KEY): 'couchbase']
-
-    void cleanupSpec() {
-        TestContainers.closeScope('couchbase')
-    }
+    private final Map<String, Object> requestedProperties = [(Scope.PROPERTY_KEY): scopeName]
 
     def "starts Couchbase and resolves supported connection properties from the same container"() {
         when:
         def uri = provider.resolve(CouchbaseTestResourceProvider.COUCHBASE_URI, requestedProperties, [:]).orElse(null)
         def username = provider.resolve(CouchbaseTestResourceProvider.COUCHBASE_USERNAME, requestedProperties, [:]).orElse(null)
         def password = provider.resolve(CouchbaseTestResourceProvider.COUCHBASE_PASSWORD, requestedProperties, [:]).orElse(null)
-        def uriContainers = TestContainers.findByRequestedProperty(Scope.of('couchbase'), CouchbaseTestResourceProvider.COUCHBASE_URI)
-        def usernameContainers = TestContainers.findByRequestedProperty(Scope.of('couchbase'), CouchbaseTestResourceProvider.COUCHBASE_USERNAME)
-        def passwordContainers = TestContainers.findByRequestedProperty(Scope.of('couchbase'), CouchbaseTestResourceProvider.COUCHBASE_PASSWORD)
+        def scope = Scope.of(scopeName)
+        def uriContainers = TestContainers.findByRequestedProperty(scope, CouchbaseTestResourceProvider.COUCHBASE_URI)
+        def usernameContainers = TestContainers.findByRequestedProperty(scope, CouchbaseTestResourceProvider.COUCHBASE_USERNAME)
+        def passwordContainers = TestContainers.findByRequestedProperty(scope, CouchbaseTestResourceProvider.COUCHBASE_PASSWORD)
 
         then:
         uri?.startsWith('couchbase://')
@@ -30,6 +27,6 @@ class CouchbaseTestResourceProviderTest extends Specification {
         uriContainers.size() == 1
         usernameContainers == uriContainers
         passwordContainers == uriContainers
-        TestContainers.listByScope('couchbase').values().flatten().size() == 1
+        TestContainers.listByScope(scopeName).values().flatten().size() == 1
     }
 }

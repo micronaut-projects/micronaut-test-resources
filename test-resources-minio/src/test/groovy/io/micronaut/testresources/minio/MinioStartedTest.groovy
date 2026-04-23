c8c49606b9ad189207cfb561bee7a77d68915389
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-04-23 14:03:29 +0200

    Add MinIO test resources support (#1048)

---
+++ test-resources-minio/src/test/groovy/io/micronaut/testresources/minio/MinioStartedTest.groovy
@@ -0,0 +1,42 @@
+package io.micronaut.testresources.minio
+
+import io.micronaut.context.annotation.Value
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.micronaut.testresources.core.Scope
+import io.micronaut.testresources.testcontainers.AbstractTestContainersSpec
+import io.micronaut.testresources.testcontainers.TestContainers
+import org.testcontainers.DockerClientFactory
+
+@MicronautTest
+class MinioStartedTest extends AbstractTestContainersSpec {
+
+    @Value('${minio.url}')
+    String url
+
+    @Value('${minio.access-key}')
+    String accessKey
+
+    @Value('${minio.secret-key}')
+    String secretKey
+
+    @Override
+    String getScopeName() {
+        'minio'
+    }
+
+    def "automatically starts a MinIO container"() {
+        given:
+        def dockerHost = DockerClientFactory.instance().dockerHostIpAddress()
+
+        expect:
+        dockerHost in ["localhost", "127.0.0.1"]
+        listContainers().size() == 1
+        url.contains(dockerHost)
+        accessKey == "minioadmin"
+        secretKey == "minioadmin"
+        with(TestContainers.listByScope("minio").get(Scope.of("minio"))) {
+            size() == 1
+            get(0).dockerImageName.startsWith(MinioTestResourceProvider.DEFAULT_IMAGE)
+        }
+    }
+}
